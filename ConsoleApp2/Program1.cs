eb0b43a907029e289a44cb6a175e85407c916c2e
Taha Kaplan <66365263+kaplanseunt@users.noreply.github.com>
2021-12-07 12:33:20 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp2/Program1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun urun1 = new Urun();
+            urun1.Aciklama = "Amasya elma";
+            urun1.Adi = "elma";
+            urun1.Fiyati = 4;
+
+            Urun urun2 = new Urun();
+            urun2.Aciklama = "Samsun Şeftalisi";
+            urun2.Adi = "Şeftali";
+            urun2.Fiyati = 12;
+
+            Urun[] urunler = new Urun[] {urun1, urun2 };
+
+            foreach (var urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine("------------");
+            }
+            Console.WriteLine(" ");
+            Console.WriteLine("----------Metotlar-----------");
+            Console.WriteLine(" ");
+            sepetManager sepetManager = new sepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+             
+
+
+        }
+    }
+}
---
+++ ConsoleApp2/sepetManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metotlar
+{
+    class sepetManager
+    {
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Sepete eklendi..." + urun.Adi);
+        }
+    }
+}
